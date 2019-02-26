5c33c46c2b92a09afa25f5960d658378246ebf36
ChristianKamalu <ckamalu98@gmail.com>
2019-02-26 15:50:43 -0700

    updated problem 9

--- practice.js
+++ practice.js
@@ -128,10 +128,10 @@ function sampleCallbackTwo() {
   return "I am also a callback function";
 }
 
-function invoker(){
-  sampleCallbackOne;
+function invoker(callBack){
+  return callBack();
 }
-
+invoker(happy)
 //////////////////PROBLEM 9////////////////////
 
 let duck = "cute";
@@ -154,16 +154,16 @@ function pond() {
 //as strings.
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let globalScope = ["duck"];
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let bathroomScope = ["duck", "rubberDuck"];
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let bathtubScope = ["duck", "sailorDuck", "rubberDuck"];
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let pondScope = ["duck", "realDuck"];
 
 //////////////////PROBLEM 10////////////////////
 
